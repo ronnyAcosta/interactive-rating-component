659847e6fe6e3d04c3eb32ba40b2b3067a1e3399
ronnyAcosta <104609124+ronnyAcosta@users.noreply.github.com>
2022-07-15 16:01:18 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let numbers = document.getElementsByClassName('numbers');
+//let selectedNumber;
+let preSelectedNumber = 0;
+
+//console.log(selectedNumber);
+function selectNumber(number){
+    for(let circle of numbers){
+        circle.style.backgroundColor='hsl(213, 19%, 23%)';
+        circle.style.color= 'hsl(216, 12%, 54%)';
+    }
+    number.style.backgroundColor='hsl(217, 12%, 63%)';
+    number.style.color='white';
+    preSelectedNumber = number.innerHTML;
+    console.log(preSelectedNumber)
+}
+function submitNumber(){
+    if(preSelectedNumber > 0){
+        let selectedNumber = document.getElementById('selected-number');
+        let ratingState = document.getElementById('rating-state');
+        let submitedState = document.getElementById('submited-state');
+        selectedNumber.innerHTML = preSelectedNumber;
+        submitedState.style.display='flex';
+        submitedState.style.flexDirection='column';
+        submitedState.style.alignItems='center';
+        ratingState.style.display='none';
+        //console.log(selectedNumber);
+    }
+    else{
+        alert("Elija un número!");
+    }
+}
